7c4a21bed6293ca72ca1ff2bd8ff7b060d39aef3
root <root@rocko.com>
2018-11-09 16:21:14 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sample app for UCV pipeline
---
+++ docker/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import os
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    html = "<html><head>" \
+           "<title>IBM Demo DevOps</title></head>" \
+           "<body style='background-color:{backgroundcolor};'>" \
+           "<p><font size='10'>IBM DevOps Demo - UrbanCode Deploy & IBM Cloud Private!Demo123!<br/></font></p>" \
+           "<p><font size='6'>Hello {name}!<br/></font></p>" \
+           "<p><font size='6'><b>Hostname:</b> {hostname}<br/></font></p>" \
+           "<p><font size='6'><b>Environment:</b> {env}<br/></font></p>" \
+           "<p><font size='6'><b>Version:</b> {version}<br/></font></p>" \
+           "</body></html>"
+    return html.format(name=os.getenv("NAME", "world"), 
+                       hostname=socket.gethostname(), 
+                       env=os.getenv("env", "Dev"), 
+                       backgroundcolor=os.getenv("backgroundcolor", "MintCream"),
+                       version=os.getenv("version", "n/a"))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
